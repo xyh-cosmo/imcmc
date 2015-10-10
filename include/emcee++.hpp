d65f52b75cefebf1a877054e193b3d2025d739a3
xyh-cosmo <yhxu@nao.cas.cn>
2015-10-10 15:27:01 +0800

    re-formatted emcee++.hpp

--- include/emcee++.hpp
+++ include/emcee++.hpp
@@ -37,9 +37,9 @@ namespace imcmc{
     class emcee_workspace{
         public:
 	        emcee_workspace();
-          ~emcee_workspace();
+            ~emcee_workspace();
 
-          std::string config_file;
+            std::string config_file;
 
 	        //	Parallel mode: 0, 1, 2
 	        //	0: serial version
@@ -55,18 +55,18 @@ namespace imcmc{
 	        std::string		chain_name;
 	        std::ofstream	out_stream;
 
-					//	whether use cosmomc standard form? "weight like param1 param2 param3 ..."
-					bool	use_cosmomc_std_format;	//	default: false. NOTE: like is actually -2*ln(L) = chisq
+			//	whether use cosmomc standard form? "weight like param1 param2 param3 ..."
+			bool	use_cosmomc_std_format;	//	default: false. NOTE: like is actually -2*ln(L) = chisq
 
-          //  filename of the parameters' limits
-          std::string     param_limits;
-          std::ofstream   param_limits_os;
+            //  filename of the parameters' limits
+            std::string     param_limits;
+            std::ofstream   param_limits_os;
 
 	        //	random numbers
-	        gsl_rng			*rand_seed;
+	        gsl_rng	*rand_seed;
 
 	        //	efficient controling parameter
-	        double			efficient_a;
+	        double	efficient_a;
 
 	        //	state of acception or rejection, 1 or 0
 	        int	total_accepts;
@@ -79,15 +79,15 @@ namespace imcmc{
 	        imcmc_double	full_param_min;		//	max values
 	        imcmc_double	full_param_max;		//	min values
 
-          //  Walkers
+            //  Walkers
 	        imcmc_double_pointer		walker;     					//  includes LnPost, LnDet and Chisq
-          imcmc_vector_string     sampling_param_name;	//	hold the names of parameters being sampled
-          imcmc_vector_string     output_param_name;  	//  if not set, then output_param_name = sampling_param_name
+            imcmc_vector_string     sampling_param_name;	//	hold the names of parameters being sampled
+            imcmc_vector_string     output_param_name;  	//  if not set, then output_param_name = sampling_param_name
 
 	        //	Likelihoof functions , contains MODEL and DATA
 	        std::vector<likelihood_*>   likelihood;
 
-	        void            set_efficient_a( double a );	//	control the range of Z, narrower Z range will increase the acceptance ratio.
+	        void    set_efficient_a( double a );	//	control the range of Z, narrower Z range will increase the acceptance ratio.
 
 					//	used to generate random variable 'Z'
 	        inline	double  g( double z );
@@ -96,40 +96,40 @@ namespace imcmc{
 	        void init( std::string infile );		//	read initialization & other settings from the input *ini file
 	        void init_param();									//	initialize relavant parameters, some might be set to default values.
 
-          bool walker_initialized;	//	inidicate whether the walkers are initialized.
+            bool walker_initialized;	//	inidicate whether the walkers are initialized.
 	        void init_walkers();			//	initialize the walkers.
-          void reset_walkers();			//	reset the walkers to a random state.
+            void reset_walkers();			//	reset the walkers to a random state.
 
-					//	=======================================================================================================================
-					//	add likelihood functions.  If you have many likelihoods functions, which share some common parameters, it'd better to
-					//	combine those likelihood functions into a BIGGER one, and adding some flags to control which likelihood functions will
-					//	be used.  This is quite common when someone is trying to constraining model parameters using different combinations of
-					//	data sets.
+			//	=======================================================================================================================
+			//	add likelihood functions.  If you have many likelihoods functions, which share some common parameters, it'd better to
+			//	combine those likelihood functions into a BIGGER one, and adding some flags to control which likelihood functions will
+			//	be used.  This is quite common when someone is trying to constraining model parameters using different combinations of
+			//	data sets.
 	        void add_likelihood( 	double (*like)( imcmc_double&, double&, double&, void*, void* ),
 			                    			imcmc_vector_string 	modelparam,
 			                    			void 					*model,
 			                    			void 					*data );
 
-	        bool 		prior( imcmc_double& full_param );	//	check Samplingparams, if out of prior range, return false
+	        bool 	prior( imcmc_double& full_param );	//	check Samplingparams, if out of prior range, return false
 
-          //  return log(posterior) = -lndet - 0.5*chisq
-          //  det is the determinat in the denominator of the prefactor, plus some constant
+            //  return log(posterior) = -lndet - 0.5*chisq
+            //  det is the determinat in the denominator of the prefactor, plus some constant
 	        double  likelihood_eval( imcmc_double& full_param, double& lndet, double& chisq );
 
-          //  these two numbers will be used to re-scale the probability, in which case the chisq is too large
-          //  so that exp(-lndet-0.5*chisq) --> 0
-          double  _lndet_min_, _chisq_min_;
-          bool    _searched_lndet_min_chisq_min_;
+            //  these two numbers will be used to re-scale the probability, in which case the chisq is too large
+            //  so that exp(-lndet-0.5*chisq) --> 0
+            double  _lndet_min_, _chisq_min_;
+            bool    _searched_lndet_min_chisq_min_;
 
-          int     update_a_walker( imcmc_double& full_param, int current_id, int rand_id );
+            int     update_a_walker( imcmc_double& full_param, int current_id, int rand_id );
 	        void    update_walkers( bool do_sampling, int ith, int num ); //	Update walkers
 	        void    write_walkers( std::ofstream& of ); //	Write walkers into text files
-          void    do_sampling();
+            void    do_sampling();
 
-					//	TODO: add some new methods
-					//	1) save walker state
-					void save_walker_state( std::ofstream& of );	//	save current walkers' state into some file
-					void read_walker_state( std::ofstream& of );	//	read the old walkers' state and then continue the MCMC sampling
+			//	TODO: add some new methods
+			//	1) save walker state
+			void save_walker_state( std::ofstream& of );	//	save current walkers' state into some file
+			void read_walker_state( std::ofstream& of );	//	read the old walkers' state and then continue the MCMC sampling
     };
 
 }
