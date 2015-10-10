63d1cb19b1c621139ba3575b64b906cb81e2e149
xyh-cosmo <yhxu@nao.cas.cn>
2015-10-10 15:39:52 +0800

    format

--- include/parser++.hpp
+++ include/parser++.hpp
@@ -60,135 +60,134 @@ namespace parser{
         double  String_to_Double( std::string str );
 
 	    //	int to string
-		  std::string Int_to_String( int a );
-		  std::string IntToString( int a );
+		std::string Int_to_String( int a );
+		std::string IntToString( int a );
+        std::string DoubleToString( double par, int precision=5 );  //  convert doubles to strings.
 
-      std::string DoubleToString( double par, int precision=5 );  //  convert doubles to strings.
+        void ToUpperCase( std::string &str ); //  convert to upper case
+        void ToLowerCase( std::string &str ); //  convert to lower case
 
-      void ToUpperCase( std::string &str ); //  convert to upper case
-      void ToLowerCase( std::string &str ); //  convert to lower case
+		bool SameStrings( std::string s1, std::string s2 );   //  compare two strings are same not not
 
-		  bool SameStrings( std::string s1, std::string s2 );
+        //  read in one string/int/double from given line by reference
+        //  NOTE: these functions should be called when there is only ONE value on the right-side of '='
+        void Read_String_from_Line( std::string line, std::string &value );
+        void Read_Bool_from_Lile( std::string line, bool &value );
+        void Read_Int_from_Line( std::string line, int &value );
+        void Read_Double_from_Line( std::string line, double &value );
 
-      //  read in one string/int/double from given line by reference
-      //  NOTE: these functions should be called when there is only ONE value on the right-side of '='
-      void Read_String_from_Line( std::string line, std::string &value );
-      void Read_Bool_from_Lile( std::string line, bool &value );
-      void Read_Int_from_Line( std::string line, int &value );
-      void Read_Double_from_Line( std::string line, double &value );
+        //  read in one string/int/double from returned value, also, must be called when there is only ONE value
+        std::string Read_String_from_Line( std::string line );
+        int         Read_Int_from_Line( std::string line );
+        double      Read_Double_from_Line( std::string line );
 
-      //  read in one string/int/double from returned value, also, must be called when there is only ONE value
-      std::string Read_String_from_Line( std::string line );
-      int         Read_Int_from_Line( std::string line );
-      double      Read_Double_from_Line( std::string line );
+        //  for array case, I recommend to use the following
+        std::string* 	Read_Array_of_String_from_Line( std::string line, int &size );
+        int* 			Read_Array_of_Int_from_Line( std::string line, int &size );
+        double* 		Read_Array_of_Double_from_Line( std::string line, int &size );
 
-      //  for array case, I recommend to use the following
-      std::string* 	Read_Array_of_String_from_Line( std::string line, int &size );
-      int* 			    Read_Array_of_Int_from_Line( std::string line, int &size );
-      double* 		  Read_Array_of_Double_from_Line( std::string line, int &size );
+        //  also, I keep these versions, but for [[known size]] of array
+        void Read_Array_of_String_from_Line( std::string line,
+						                     std::string str_array[],
+						                     int         size,
+						                     bool        warn=false	);
 
-      //  also, I keep these versions, but for [[known size]] of array
-      void Read_Array_of_String_from_Line( std::string line,
-												                   std::string str_array[],
-												                   int         size,
-												                   bool        warn=false	);
+        void Read_Array_of_Int_from_Line( std::string  line,
+										  int          int_array[],
+										  int          size,
+										  bool         warn=false	);
 
-      void Read_Array_of_Int_from_Line(	std::string  line,
-											                  int          int_array[],
-											                  int          size,
-											                  bool         warn=false	);
+        void Read_Array_of_Double_from_Line( std::string  line,
+											 double       double_array[],
+											 int          size,
+											 bool         warn=false );
 
-      void Read_Array_of_Double_from_Line( std::string  line,
-												                   double       double_array[],
-												                   int          size,
-												                   bool         warn=false );
+        //	============================================================================================
+        //  read int a single value
+        void Read_Value_from_File( std::string infile, std::string key, std::string& value );
+        void Read_Value_from_File( std::string infile, std::string key, int& value );
+        void Read_Value_from_File( std::string infile, std::string key, double& value );
 
-      //	============================================================================================
-      //  read int a single value
-      void Read_Value_from_File( std::string infile, std::string key, std::string& value );
-      void Read_Value_from_File( std::string infile, std::string key, int& value );
-      void Read_Value_from_File( std::string infile, std::string key, double& value );
-
-      //	void Read_Value_from_File( std::string infile, std::string key, bool& value );	//	this one is not necessary, since we always use strings "true" / "false" in *.ini files
+        //	void Read_Value_from_File( std::string infile, std::string key, bool& value );	//	this one is not necessary, since we always use strings "true" / "false" in *.ini files
 
 	    //	BRead_*_*_*
 	    //	'B' means return a bool value
-      bool BRead_Value_from_File( std::string infile, std::string key, std::string &value );
-      bool BRead_Value_from_File( std::string infile, std::string key, int &value );
-      bool BRead_Value_from_File( std::string infile, std::string key, double &value );
+        bool BRead_Value_from_File( std::string infile, std::string key, std::string &value );
+        bool BRead_Value_from_File( std::string infile, std::string key, int &value );
+        bool BRead_Value_from_File( std::string infile, std::string key, double &value );
 
-		  std::string Read_String_from_File(std::string infile, std::string key );
-      bool        Read_Bool_from_File( std::string infile, std::string key );
-      int 		    Read_Int_from_File( std::string infile, std::string key );
-      double 		  Read_Double_from_File( std::string infile, std::string key );
+		std::string Read_String_from_File(std::string infile, std::string key );
+        bool        Read_Bool_from_File( std::string infile, std::string key );
+        int 		Read_Int_from_File( std::string infile, std::string key );
+        double 		Read_Double_from_File( std::string infile, std::string key );
 
-      //  read in an array of values, and the number of readed values will be also returned
-		  std::string* 	Read_Array_of_String_from_File( std::string infile,
-														                        std::string key,
-														                        int         &array_size );
+        //  read in an array of values, and the number of readed values will be also returned
+		std::string* 	Read_Array_of_String_from_File( std::string infile,
+							                            std::string key,
+														int         &array_size );
 
-		  int* 			    Read_Array_of_Int_from_File( std::string infile,
-														                     std::string key,
-														                     int         &array_size	);
+		int* 			Read_Array_of_Int_from_File( std::string infile,
+								                     std::string key,
+								                     int         &array_size	);
 
-		  double* 		  Read_Array_of_Double_from_File( std::string infile,
-														                        std::string key,
-														                        int         &array_size );
+		double* 		Read_Array_of_Double_from_File( std::string infile,
+								                        std::string key,
+                                                        int         &array_size );
 
-      //  for know size
-      void Read_Array_from_File( std::string infile,
-								                 std::string key,
-								                 std::string array[],
-								                 int         array_size	);
+        //  for know size
+        void Read_Array_from_File( std::string infile,
+                                   std::string key,
+                                   std::string array[],
+                                   int         array_size	);
 
-      void Read_Array_from_File( std::string infile,
-								                 std::string key,
-                                 int         array[],
-                                 int         array_size);
+        void Read_Array_from_File( std::string infile,
+                                   std::string key,
+                                   int         array[],
+                                   int         array_size);
 
-      void Read_Array_from_File( std::string infile,
-                                 std::string key,
-                                 double      array[],
-                                 int         array_size	);
+        void Read_Array_from_File( std::string infile,
+                                   std::string key,
+                                   double      array[],
+                                   int         array_size	);
 
-      //  modified version for given size of array
-		  void Read_Array_of_String_from_File(  std::string infile,
-                                            std::string key,
-                                            std::string str_array[],
-                                            int         array_size	);
+        //  modified version for given size of array
+		void Read_Array_of_String_from_File(  std::string infile,
+                                              std::string key,
+                                              std::string str_array[],
+                                              int         array_size	);
 
-      void Read_Array_of_Int_from_File( std::string infile,
-                                        std::string key,
-                                        int         int_array[],
-                                        int         array_size );
+        void Read_Array_of_Int_from_File( std::string infile,
+                                          std::string key,
+                                          int         int_array[],
+                                          int         array_size );
 
-		  void Read_Array_of_Double_from_File( std::string infile,
-												                   std::string key,
-                                           double      double_array[],
-                                           int         array_size );
+		void Read_Array_of_Double_from_File( std::string infile,
+                                             std::string key,
+                                             double      double_array[],
+                                             int         array_size );
 
-      //	BRead_*_*_*
+        //	BRead_*_*_*
 	    //	B means return a bool value
-      bool BRead_Array_from_File(	std::string infile,
-									                std::string key,
-                                  std::string array[],
-                                  int         array_size	);
+        bool BRead_Array_from_File(	std::string infile,
+                                    std::string key,
+                                    std::string array[],
+                                    int         array_size	);
 
-      bool BRead_Array_from_File(	std::string infile,
-									                std::string key,
-                                  int         array[],
-                                  int         array_size	);
+        bool BRead_Array_from_File(	std::string infile,
+                                    std::string key,
+                                    int         array[],
+                                    int         array_size	);
 
-      bool BRead_Array_from_File(	std::string infile,
-									                std::string key,
-                                  double      array[],
-                                  int         array_size	);
-      }
+        bool BRead_Array_from_File(	std::string infile,
+                                    std::string key,
+                                    double      array[],
+                                    int         array_size	);
+    }
 
-      namespace Write{
-          void WarningInfo( std::string funcname, std::string warninginfo, bool stop=false );
-          void ErrorInfo( std::string funcname, std::string errorinfo );
-      }
+    namespace Write{
+        void WarningInfo( std::string funcname, std::string warninginfo, bool stop=false );
+        void ErrorInfo( std::string funcname, std::string errorinfo );
+    }
 }
 
 }
