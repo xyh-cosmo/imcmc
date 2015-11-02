0a808f04b8262223eed22324985c1eb90cc8620f
xyh-cosmo <yhxu@nao.cas.cn>
2015-11-02 17:43:48 +0800

    ...

--- src/ensemble_init.cpp
+++ src/ensemble_init.cpp
@@ -496,7 +496,7 @@ namespace imcmc{
             outfile << "# =========================================\n";
 
         //    derived_params_width >= params_width !!
-            outfile << std::setw(derived_params_width) << std::left <<" params:" << ""
+            outfile << std::setw(derived_params_width) << std::left <<" params:" << " "
                     << std::setw(15) << std::right << "min-value" << " "
                     << std::setw(15) << std::right << "max-value" << " "
                     << std::setw(15) << std::right << "do sampling?" << "\n";
